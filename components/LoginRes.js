9997053a57469039b5f1e704642f02e3cc35a64a
Bagaskoro Jati Waluyo <bagaskoro125@gmail.com>
2022-10-16 19:49:13 +0700

    add github provider

--- components/LoginRes.js
+++ components/LoginRes.js
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { useRef } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { closeLoginRes, selectLoginRes, setLoginResView } from "../slices/popupSlice";
@@ -34,12 +35,14 @@ export default function LoginRes(){
                     </svg>
                     <span className="font-bold flex-1 self-center">Continue with Google</span>
                 </button>
-                <button className="flex items-center bg-gray-100 hover:bg-gray-200 transition w-full px-6 rounded-lg py-3 mb-4">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-github" viewBox="0 0 16 16">
-                        <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z"/>
-                    </svg>
-                    <span className="font-bold flex-1 self-center">Continue with Github</span>
-                </button>
+                <Link href={`${process.env.NEXT_PUBLIC_API}/api/connect/github`}>
+                    <button className="flex items-center bg-gray-100 hover:bg-gray-200 transition w-full px-6 rounded-lg py-3 mb-4">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-github" viewBox="0 0 16 16">
+                            <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z"/>
+                        </svg>
+                        <span className="font-bold flex-1 self-center">Continue with Github</span>
+                    </button>
+                </Link>
                 <button onClick={showLoginRegister} className="transition w-full px-6 rounded-lg py-3 mb-6 bg-blue-500 font-bold text-white hover:bg-blue-600">Continue with email</button>
                 <p className="text-gray-500 text-xs text-center">By continuing, you agree to our <span className="text-black">Terms of Service.</span></p>
             </div>
---
+++ pages/api/auth/[provider].js
@@ -0,0 +1,10 @@
+import axios from "axios";
+import { serialize  } from "cookie";
+
+export default async function handler(req, res){
+    const { access_token, provider } = req.query;
+    const request = await axios.get(`${process.env.NEXT_PUBLIC_API}/api/auth/${provider}/callback?access_token=${access_token}`);
+    console.log(request.data)
+    res.setHeader('Set-Cookie', serialize('token', request.data.jwt, { path: '/', maxAge: 30 * 24 * 60 * 60 }));
+    res.redirect('/');
+}
